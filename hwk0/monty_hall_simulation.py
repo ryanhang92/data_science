b41e98ffe8bb69adb1e7e3ecf61f400df5aa451c
ryanhang92 <ryanhang92@gmail.com>
2017-07-24 00:09:27 -0700

    declaring questions

---
+++ hwk0/monty_hall_simulation.py
@@ -0,0 +1,164 @@
+"""
+Function
+--------
+simulate_prizedoor
+
+Generate a random array of 0s, 1s, and 2s, representing
+hiding a prize between door 0, door 1, and door 2
+
+Parameters
+----------
+nsim : int
+    The number of simulations to run
+
+Returns
+-------
+sims : array
+    Random array of 0s, 1s, and 2s
+
+Example
+-------
+#>>> print simulate_prizedoor(3)
+array([0, 0, 2])
+"""
+def simulate_prizedoor(nsim):
+    #compute here
+    return answer
+
+
+
+
+
+
+"""
+Function
+--------
+simulate_prizedoor
+
+Generate a random array of 0s, 1s, and 2s, representing
+hiding a prize between door 0, door 1, and door 2
+
+Parameters
+----------
+nsim : int
+    The number of simulations to run
+
+Returns
+-------
+sims : array
+    Random array of 0s, 1s, and 2s
+
+Example
+-------
+>>> print simulate_prizedoor(3)
+array([0, 0, 2])
+"""
+def simulate_prizedoor(nsim):
+    #compute here
+    return answer
+#your code here
+
+
+
+
+
+
+
+"""
+Function
+--------
+goat_door
+
+Simulate the opening of a "goat door" that doesn't contain the prize,
+and is different from the contestants guess
+
+Parameters
+----------
+prizedoors : array
+    The door that the prize is behind in each simulation
+guesses : array
+    THe door that the contestant guessed in each simulation
+
+Returns
+-------
+goats : array
+    The goat door that is opened for each simulation. Each item is 0, 1, or 2, and is different
+    from both prizedoors and guesses
+
+Examples
+--------
+>>> print goat_door(np.array([0, 1, 2]), np.array([1, 1, 1]))
+>>> array([2, 2, 0])
+"""
+#your code here
+
+
+
+
+
+
+"""
+Function
+--------
+switch_guess
+
+The strategy that always switches a guess after the goat door is opened
+
+Parameters
+----------
+guesses : array
+     Array of original guesses, for each simulation
+goatdoors : array
+     Array of revealed goat doors for each simulation
+
+Returns
+-------
+The new door after switching. Should be different from both guesses and goatdoors
+
+Examples
+--------
+>>> print switch_guess(np.array([0, 1, 2]), np.array([1, 2, 1]))
+>>> array([2, 0, 0])
+"""
+#your code here
+
+
+
+
+"""
+Function
+--------
+win_percentage
+
+Calculate the percent of times that a simulation of guesses is correct
+
+Parameters
+-----------
+guesses : array
+    Guesses for each simulation
+prizedoors : array
+    Location of prize for each simulation
+
+Returns
+--------
+percentage : number between 0 and 100
+    The win percentage
+
+Examples
+---------
+>>> print win_percentage(np.array([0, 1, 2]), np.array([0, 0, 0]))
+33.333
+"""
+#your code here
+
+
+
+
+
+
+'''
+Now, put it together. Simulate 10000 games where contestant keeps his original guess, and 10000 games where the contestant switches his door after a goat door is revealed. Compute the percentage of time the contestant wins under either strategy. Is one strategy better than the other?
+'''
+#your code here
+
+
