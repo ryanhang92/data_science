054100b8f291230b860cb38e9acc6d5aa219f6d3
ryanhang92 <ryanhang92@gmail.com>
2017-09-15 00:48:17 -0700

    completing fetching zipped csv data

--- hwk1/sport_salary.py
+++ hwk1/sport_salary.py
@@ -8,31 +8,33 @@ from zipfile import ZipFile
 #Read from the web
 zip_url = "http://seanlahman.com/files/database/lahman-csv_2014-02-14.zip"
 
+#Wtih url library
 url = urlopen(zip_url)
 zipfile = ZipFile(StringIO(url.read()))
-
 #with zipfile.ZipFile('archive.zip') as z:
 with zipfile as z:
     for filename in z.namelist():
-        with z.open(filename) as f:
-            for line in f:
-                print line
+        print filename
+        if filename == "Salaries.csv":
+            with z.open(filename) as f:
+                for line in f:
+                    print line
 
-'''
-url = urlopen(zip_url)
-zipfile = ZipFile(StringIO(url.read()))
-zipfile.infolist()
-for line in zipfile.open(file).readlines():
-    print line
-
+#With Requests Library
 r = requests.get(zip_url)
-print r
-zipfile = ZipFile(StringIO(url.read()))
-for line in zipfile.open(file).readlines():
-    print line
-'''
+f = StringIO()
+f.write(r.content)
+def extract_zip(input_zip):
+    zipped_file = ZipFile(input_zip)
+    for filename in zipped_file.namelist():
+        if filename == "Salaries.csv":
+            with zipped_file.open(filename) as f:
+                for line in f:
+                    print line
+    #return {i: input_zip.read(i) for i in input_zip.namelist()}
+extracted = extract_zip(f)
 
-'''
+
 #Read file from path
 salary_path = 'data/Salaries.csv'
 with open(salary_path, 'rb') as csvfile:
@@ -45,10 +47,8 @@ with open(teams_path, 'rb') as csvfile:
     teams_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in teams_reader:
         print row
-'''
-#Filter data in memory to put into an array and then a np array do a dict of lists approach
 
-
+#Process the data once in the right data structure
 
 
 
