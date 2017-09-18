6ffb8acd5f7a9dcad3400329cf52d5eadeb72737
ryanhang92 <ryanhang92@gmail.com>
2017-09-18 00:53:03 -0700

    preping merge of dataframes

--- hwk1/sport_salary.py
+++ hwk1/sport_salary.py
@@ -53,24 +53,40 @@ with open(teams_path, 'rb') as csvfile:
 
 
 # Problem 1(b) Summarize the Salaries DataFrame to show the total salaries for each team for each year. Show the head of the new summarized DataFrame.
+
 #Read file from path
+'''
 salary_path = 'data/Salaries.csv'
 with open(salary_path, 'rb') as csvfile:
     salary_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for row in salary_reader:
-        print row
-
 
     #Create a feature vector [team, year, salary)
+    feature_vectors = []
+    for i, row in enumerate(salary_reader):
+        if i > 0:
+            team_name = row[1]
+            year = int(row[0])
+            salary = int(row[4])
+            feature_vector = np.array([team_name, year, salary])
+            feature_vectors.append(feature_vector)
 
-
+    print feature_vectors
+'''
 
 
 # Problem 1(c) Merge the new summarized Salaries DataFrame and Teams DataFrame together to create a new DataFrame showing wins and total salaries for each team for each year year. Show the head of the new merged DataFrame.
 #Hint: Merge the DataFrames using teamID and yearID.
+teams_path = 'data/Teams.csv'
+with open(teams_path, 'rb') as csvfile:
+    teams_reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for i, row in enumerate(teams_reader):
+        if i == 1 or i == 0:
+            print row
 
 
-    #Create a feature vector [team, year, salary, wins)
+
+    #Create a feature vector [team, year, salary, wins) - creating a lookup for team and year, vlaue is wins and appeending wins to feature vector
+
 
 
 
@@ -78,12 +94,15 @@ with open(salary_path, 'rb') as csvfile:
 #Hints: Use a for loop to consider multiple years. Use the teamID (three letter representation of the team name) to save space on the plot.
 
 
+
     #For each year
         #Show each team as a point, graph salary on x axis and wins on y axis as y is the dependent variable
 
 
 
+
 #Problem 1(e): For AC209 Students: Fit a linear regression to the data from each year and obtain the residuals. Plot the residuals against time to detect patterns that support your answer in 1(d).
+
 
     #For each year
         #Train linear classifier where the features are just the salary, and label is wins
